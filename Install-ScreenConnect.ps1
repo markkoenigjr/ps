980583b73f09e33c379e0b95c7b79a92282c6044
markkoenigjr <mkoenig@hu-friedy.com>
2025-04-19 05:39:26 -0700

    Update Install-ScreenConnect.ps1

--- Install-ScreenConnect.ps1
+++ Install-ScreenConnect.ps1
@@ -1,14 +1,35 @@
 #!ps
 #timeout=1000000
 
-# Variables
-$fileName       = "sc.exe"
-$folderPath     = Join-Path -Path $PWD -ChildPath "ScreenConnect"
-$fullFilePath   = Join-Path -Path $folderPath -ChildPath $fileName
-$uri            = "https://5555.screenconnect.com/Bin/ScreenConnect.ClientSetup.exe?e=Access&y=Guest"
+# Constants
+$fileName       = $null
+$folderPath     = $null
+$fullFilePath   = $null
+$uri            = $null
+
+if ($IsWindows) {
+    $root           = [System.IO.Path]::GetPathRoot($PWD)
+    $fileExtension  = "exe"
+    $fileName       = "sc.$fileExtension"
+    $folderPath     = Join-Path -Path $root -ChildPath "ScreenConnect"
+    $fullFilePath   = Join-Path -Path $folderPath -ChildPath $fileName
+    $uri            = "https://5555.screenconnect.com/Bin/ScreenConnect.ClientSetup.$($fileExtension)?e=Access&y=Guest"
+}
+
+# Incorporate MacOS and Linux installation routines.
+if ($IsMacOS -Or $IsLinux) {
+    $root           = "/"
+    $fileExtension  = "sh"
+    $fileName       = "sc.$fileExtension"
+    $folderPath     = Join-Path -Path $root -ChildPath "ScreenConnect"
+    $fullFilePath   = Join-Path -Path $folderPath -ChildPath $fileName
+    $uri            = "https://5555.screenconnect.com/Bin/ScreenConnect.ClientSetup.$($fileExtension)?e=Access&y=Guest"
+}
+
+Write-Host $uri
 
 # Create the folder path
-New-Item -ItemType Directory -Path $folderPath
+New-Item -ItemType Directory -Path $folderPath -Force
 
 # Download the file
 Invoke-WebRequest -Uri $uri -OutFile $fullFilePath
