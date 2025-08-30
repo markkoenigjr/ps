58c2de4a42c51d9c683b61356e7bccedba956825
markkoenigjr <mkoenig@hu-friedy.com>
2025-08-29 17:52:39 -0700

    Create Get-WindowsUpdate.ps1
    List available Windows updates.

---
+++ Get-WindowsUpdate.ps1
@@ -0,0 +1,19 @@
+#!ps
+#timeout=100000000
+
+# Create the update session
+$UpdateSession  = New-Object -ComObject Microsoft.Update.Session
+$UpdateSearcher = $UpdateSession.CreateUpdateSearcher()
+
+Write-Host "Searching for updates. Please be patient."
+$Updates        = @($UpdateSearcher.Search("IsHidden=0").Updates)
+
+$Updates        = $null
+
+# List available updates
+if ($Updates) {
+    Write-Host "`nUPDATES AVAILABLE"
+    $Updates | Where-Object { !$_.IsInstalled } | Select-Object Title | Format-Table
+} else {
+    Write-Host "`nNo updates found."
+}
